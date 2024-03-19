10a5a710d8080303de2525f192ee410cd762c383
Eric <longeric464@gmail.com>
2024-03-19 14:12:33 -0700

    initial commit

---
+++ API/requests.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const cors = require('cors')
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql2/promise');
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+require('dotenv').config();
+
+const port = process.env.SERVER_PORT;
+
+const app = express();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+app.use(async function (req, res, next) {
+    try {
+        req.db = await pool.getConnection();
+        req.db.connection.config.namedPlaceholders = true;
+
+        await req.db.query(`SET SESSION sql_mode = "TRADITIONAL"`);
+        await req.db.query(`SET time_zone = '-8:00'`);
+
+        await next();
+
+        req.db.release();
+    } catch (err) {
+        console.log(err);
+
+        if (req.db) req.db.release();
+        throw err;
+    }
+});
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(cors({
+    origin: `http://localhost:${process.env.CLIENT_PORT}`,
+    credentials: true,
+}));
+
+
+app.use((req, res, next) => {
+    res.secureCookie = (name, val, options = {}) => {
+        res.cookie(name, val, {
+            sameSite: "strict",
+            httpOnly: true,
+            secure: true,
+            ...options,
+        });
+    };
+    next();
+});
+
+function authenticateToken(req, res, next) {
+    const token = req.cookies.token
+    if (token == null) { return res.sendStatus(401) };
+
+    jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+        if (err) { console.log(err); return res.sendStatus(403) }
+        req.user = user;
+        next()
+    })
+}
+
+app.use(authenticateToken);
+
+//Functions
+//retrieves users id from the stored cookie
+async function findUID(userObj, req) {
+    const [[queriedUser]] = await req.db.query(
+        `SELECT * FROM users WHERE email = :userEmail AND password = :userPW AND deleted = 0`,
+        {
+            "userEmail": userObj.email,
+            "userPW": userObj.securePassword
+        }
+    );
+    return queriedUser.id
+}
+
+//finds a target id from an email
+async function findTargetUID(targetEmail, req) {
+    const [[queriedUser]] = await req.db.query(
+        `SELECT * FROM users WHERE email = :userEmail AND deleted = 0`,
+        {
+            "userEmail": targetEmail,
+        }
+    );
+    return queriedUser.id
+}
+
+//finds team id from uid
+async function findTeamID(teamUID, teamName, req) {
+    const [[queriedTeam]] = await req.db.query(
+        `SELECT * FROM teams WHERE uid = :uid and name = :teamName`,
+        {
+            uid: teamUID,
+            teamName: teamName
+        }
+    );
+    return queriedTeam.id
+}
+
+//checks if user is the owner of the team, outputs true or false
+async function verifyTeamOwner(teamUID, userID, req) {
+    const [queryList] = await req.db.query(`
+    SELECT * FROM teams where uid = :uid AND ownerID = :ownerID AND deleted = false`,
+        {
+            uid: teamUID,
+            ownerID: userID
+        })
+
+    if (queryList.length) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+//Endpoints
+
+
+//Listener
+
+app.listen(port, () => console.log(`Server listening on http://localhost:${process.env.SERVER_PORT}`));
