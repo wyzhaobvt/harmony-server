351c96d571142882565226a0005414236b078fc4
Nolan Lawler <nlawler0207@gmail.com>
2024-05-01 17:28:33 -0700

    removed unused code, send live updates

--- Database/requests.js
+++ Database/requests.js
@@ -1,74 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors')
-const jwt = require('jsonwebtoken');
-const mysql = require('mysql2/promise');
-const cookieParser = require("cookie-parser");
-const bcrypt = require("bcrypt");
 const router = express.Router()
-
-const port = process.env.SERVER_PORT;
-
-const app = express();
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-router.use(async function (req, res, next) {
-    try {
-        req.db = await pool.getConnection();
-        req.db.connection.config.namedPlaceholders = true;
-
-        await req.db.query(`SET SESSION sql_mode = "TRADITIONAL"`);
-        await req.db.query(`SET time_zone = '-8:00'`);
-
-        await next();
-
-        req.db.release();
-    } catch (err) {
-        console.log(err);
-
-        if (req.db) req.db.release();
-        throw err;
-    }
-});
-
-router.use(express.json());
-router.use(cookieParser())
-router.use(cors({
-    origin: `http://localhost:${process.env.CLIENT_PORT}`,
-    credentials: true,
-}));
-
+const { sockets } = require("../Peer/sockets.cjs");
 
 router.use((req, res, next) => {
-    res.secureCookie = (name, val, options = {}) => {
-        res.cookie(name, val, {
-            sameSite: "strict",
-            httpOnly: true,
-            secure: true,
-            ...options,
-        });
-    };
-    next();
+  req.socket = sockets.get(req.user.email).socket;
+  next();
 });
-
-function authenticateToken(req, res, next) {
-    const token = req.cookies.token
-    if (token == null) { return res.sendStatus(401) };
-
-    jwt.verify(token, process.env.JWT_KEY, (err, user) => {
-        if (err) { console.log(err); return res.sendStatus(403) }
-        req.user = user;
-        next()
-    })
-}
-
-router.use(authenticateToken);
 
 //Functions
 //retrieves users id from the stored cookie
@@ -356,6 +294,7 @@ router.post("/createFriendRequest", async function (req, res) {
         let requestUID = Array.from(Array(254), () => Math.floor(Math.random() * 36).toString(36)).join("")
         const targetID = await findTargetUID(req.body.targetEmail, req)
         const isUserFriendsWithTarget = await checkUserFriendsWithTarget(targetID, userID, req)
+
         if (isUserFriendsWithTarget.result) {
             res.status(400).json({ success: false, message: isUserFriendsWithTarget.message })
             return
@@ -372,13 +311,17 @@ router.post("/createFriendRequest", async function (req, res) {
         }
 
         await req.db.query(`
-        INSERT INTO requests (uid , senderID , recieverID , operation , status , deleted)
-        VALUES (:uid , :senderID , :recieverID , "addFriend" , "pending" , false);`,
-            {
-                uid: requestUID,
-                senderID: userID,
-                recieverID: targetID,
-            })
+          INSERT INTO requests (uid , senderID , recieverID , operation , status , deleted)
+          VALUES (:uid , :senderID , :recieverID , "addFriend" , "pending" , false);`,
+          {
+              uid: requestUID,
+              senderID: userID,
+              recieverID: targetID,
+          }
+        )
+
+        const receivingSocket = sockets.get(req.body.targetEmail).socket
+        receivingSocket.emit("update:new_friend_request", {username: req.user.username || req.user.email})
 
         res.status(200).json({ success: true })
     } catch (error) {
@@ -412,6 +355,21 @@ router.post("/resolveIncomingFriendRequest", async function (req, res) {
         //Accept/Deny Check
         const acceptedCheck = req.body.accepted
 
+        const [[{senderID: targetID}]] = await req.db.query(
+          `SELECT senderID FROM requests WHERE uid = :requestUID`,
+          {
+            requestUID: req.body.requestUID
+          }
+        )
+
+        
+        const [[{email: targetEmail}]] = await req.db.query(
+          `SELECT email FROM users WHERE id = :targetID`,
+          {
+            targetID: targetID
+          }
+        )
+
         //Deny Resolve
         if (!acceptedCheck) {
             await req.db.query(
@@ -422,6 +380,9 @@ router.post("/resolveIncomingFriendRequest", async function (req, res) {
                     requestUID: req.body.requestUID
                 }
             )
+
+            req.socket.to(sockets.get(targetEmail).socket.id).emit("update:reject_friend_request", {username: req.user.username || req.user.email})
+
             res.status(200).json({ success: true })
             return
         }
@@ -455,6 +416,10 @@ router.post("/resolveIncomingFriendRequest", async function (req, res) {
                     requestUID: req.body.requestUID
                 }
             )
+
+            req.socket.to(sockets.get(targetEmail).socket.id).emit("update:accept_friend_request", {username: req.user.username || req.user.email})
+
+            req.socket.emit("update:accept_friend_request", {username: req.user.username || req.user.email})
 
             res.status(200).json({ "success": true })
         }
